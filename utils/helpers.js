e5d71b994c85d1d13a5bf9d613b1b0c063cad1ea
Kash <kashay@agyeman.me>
2021-03-21 16:19:13 -0700

    helpers added

---
+++ utils/helpers.js
@@ -0,0 +1,14 @@
+// Formats the date to be more readable for  user on every post/comment.
+module.exports = {
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+  },
+  format_plural: (word, amount) => {
+      if (amount !== 1) {
+        return `${word}s`;
+      }
+      return word;
+  }
+}
